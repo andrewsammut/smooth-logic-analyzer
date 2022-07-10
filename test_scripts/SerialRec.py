d83cb3d0154bea216d76762fda8d9ac7656a57dd
gr8gregory <gregory.huras@gmail.com>
2022-07-10 19:43:34 +0000

    Updated files to enable reading from USB source and display to LCD

---
+++ test_scripts/SerialRec.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import time
+import serial
+import sys
+import os
+
+os.remove('example.txt')
+
+ser = serial.Serial(
+        port='/dev/ttyUSB0',
+        baudrate = 115200,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
+        timeout=1
+)
+
+xcount = 0
+
+while 1:
+    # Reads one byte of informaiton constantly polling the serial port
+    r=ser.read(8)
+    
+    # Blocks the '' instances of reading nothing
+    if r > bytes(0b1):
+        ex = open('data.txt', 'a')
+        
+        #Prints what was read from the serial port
+        print(r)
+
+        #Convert the Binary value of "bytes" data type to an int binary
+        h = int(r,2)
+        count = 0 
+
+        # Loop through and split up the byte into its individual 1's and 0's and 
+        # store in a file each 1 and zero on seperate lines
+        for power in [1,2,4,8,16,32,64,128]:
+            
+            s = (str(xcount) + "," + str((h&(0x1<<count))/(power)) + "\n")
+            ex.write(s)
+            count = count + 1
+            xcount = xcount + 1 
+        ex.close()
+
+    #else
+        #s = (str(xcount) + "," + "0"        
+            
--- test_scripts/live_graph.py
+++ test_scripts/live_graph.py
@@ -1,6 +1,11 @@
+#!/usr/bin/env python
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from matplotlib import style
+import serial
+
+WINDOWSIZE = 50
+
 
 style.use('fivethirtyeight')
 
@@ -8,7 +13,8 @@ fig = plt.figure()
 ax1 = fig.add_subplot(1,1,1)
 
 def animate(i):
-    graph_data = open('example.txt','r').read()
+    
+    graph_data = open('data.txt','r').read()
     lines = graph_data.split('\n')
     xs = []
     ys = []
@@ -18,7 +24,11 @@ def animate(i):
             xs.append(float(x))
             ys.append(float(y))
     ax1.clear()
-    ax1.plot(xs, ys)
+    ax1.plot(xs, ys, drawstyle='steps-pre')
+    if (float(x) - xs[0]) < WINDOWSIZE:
+        plt.xlim([0,float(x)])
+    else:
+        plt.xlim([(float(x)-WINDOWSIZE), float(x)])
 
-ani = animation.FuncAnimation(fig, animate, interval=1000)
+ani = animation.FuncAnimation(fig, animate, interval=1)
 plt.show()
